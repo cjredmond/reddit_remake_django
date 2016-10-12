477b05ce140c8c18b2711e2c7addded11c81a130
cjredmond <connor.redmond@gmail.com>
2016-10-12 14:40:30 -0400

    before migration

--- reddit_remake/app/admin.py
+++ reddit_remake/app/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from app.models import Post,Subreddit,Comment
 
+admin.site.register([Post, Subreddit, Comment])
 # Register your models here.
--- reddit_remake/app/models.py
+++ reddit_remake/app/models.py
@@ -1,3 +1,42 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Subreddit(models.Model):
+    name = models.CharField(max_length = 40)
+    description = models.CharField(max_length = 40)
+    creation_time = models.DateTimeField(auto_now_add=True)
+
+    def current_count(self):
+        posts = Post.objects.filter(subreddit = self)
+        count = 0
+        for post in posts:
+            count += 1
+        return count
+
+    def today_count(self):
+        pass
+    def daily_avg(self):
+        pass
+
+
+class Post(models.Model):
+    title = models.CharField(max_length = 40)
+    description =  models.CharField(max_length = 255)
+    body = models.TextField()
+    url = models.URLField(max_length = 250)
+    creation_time = models.DateTimeField(auto_now_add=True)
+    modification_time = models.DateTimeField(auto_now=True)
+    subreddit = models.ForeignKey(Subreddit)
+    user = models.ForeignKey(User, null=True, blank = True)
+
+    def is_recent(self):
+        pass
+    def is_hot(self):
+        pass
+
+class Comment(models.Model):
+    text = models.TextField()
+    user = models.ForeignKey(User, null=True, blank = True)
+    post = models.ForeignKey(Post)
+    created_time = models.DateTimeField(auto_now_add=True)
+    modified_time = models.DateTimeField(auto_now=True)
